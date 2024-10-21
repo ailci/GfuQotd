e46d257b28cce268cd7682edb2c46b51325a58e2
Ali Ilci <Ali.Ilci@gmx.de>
2024-10-21 14:29:47 +0200

    Qotd

--- GfuQotd.Blazor.Wasm/Pages/Home.razor.cs
+++ GfuQotd.Blazor.Wasm/Pages/Home.razor.cs
@@ -1,5 +1,13 @@
+using GfuQotd.Shared.Model;
+
 namespace GfuQotd.Blazor.Wasm.Pages;
 public partial class Home
 {
+    public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 
+    //protected override void OnInitialized()
+    //{
+    //    QotdViewModel = new QuoteOfTheDayViewModel("Larum lierum Löffelstiel", "Ich", "Dozent",
+    //        DateOnly.FromDateTime(DateTime.Now), null, null);
+    //}
 }
---
+++ GfuQotd.Shared/Model/QuoteOfTheViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GfuQotd.Shared.Model;
+
+public record QuoteOfTheDayViewModel(string QuoteText, string AuthorName, string AuthorDescription,
+    DateOnly? AuthorBirthdate, byte[]? AuthorPhoto, string? AuthorPhotoMimeType);
