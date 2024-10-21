13a99a162857946247afba52d1a2867393d3e7c3
Ali Ilci <Ali.Ilci@gmx.de>
2024-10-21 15:44:21 +0200

    HttpClient

--- GfuQotd.Blazor.Wasm/Pages/Home.razor.cs
+++ GfuQotd.Blazor.Wasm/Pages/Home.razor.cs
@@ -2,10 +2,14 @@ using GfuQotd.Shared.Model;
 using Microsoft.AspNetCore.Components;
 
 namespace GfuQotd.Blazor.Wasm.Pages;
+
+
 public partial class Home
 {
     [Inject] public ILogger<Home> Logger { get; set; } = default!;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
+
+    [Inject] public IHttpClientFactory HttpClientFactory { get; set; } = default!;
 
     //protected override void OnInitialized()
     //{
@@ -14,4 +18,17 @@ public partial class Home
     //    QotdViewModel = new QuoteOfTheDayViewModel("Larum lierum Löffelstiel", "Ich", "Dozent",
     //        DateOnly.FromDateTime(DateTime.Now), null, null);
     //}
+
+    protected override async Task OnInitializedAsync()
+    {
+        Logger.LogInformation("Home Componente aufgerufen...");
+
+        // 1. Version Mit HttpClientFactory
+        using var client = HttpClientFactory.CreateClient("qotdapiservice");
+        var response = await client.GetAsync("authors/quotes/random");
+        response.EnsureSuccessStatusCode();
+        var json = await response.Content.ReadAsStringAsync();
+        Logger.LogInformation($"Response => {json}");
+
+    }
 }
--- GfuQotd.Blazor.Wasm/Program.cs
+++ GfuQotd.Blazor.Wasm/Program.cs
@@ -1,11 +1,22 @@
 using GfuQotd.Blazor.Wasm;
+using GfuQotd.Blazor.Wasm.Pages;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//HttpClient Named Client
+builder.Services.AddHttpClient("qotdapiservice", opt =>
+{
+    opt.BaseAddress = new Uri("https://qotdminimalapi.azurewebsites.net");
+});
+
+
+
+//DI
+
 
 await builder.Build().RunAsync();
