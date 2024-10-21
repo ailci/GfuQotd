c8447aa31debdd3e505e00b15fe346ae1a732a6e
Ali Ilci <Ali.Ilci@gmx.de>
2024-10-21 14:38:28 +0200

    Home

--- GfuQotd.Blazor.Wasm/Pages/Home.razor.cs
+++ GfuQotd.Blazor.Wasm/Pages/Home.razor.cs
@@ -1,12 +1,16 @@
 using GfuQotd.Shared.Model;
+using Microsoft.AspNetCore.Components;
 
 namespace GfuQotd.Blazor.Wasm.Pages;
 public partial class Home
 {
+    [Inject] public ILogger<Home> Logger { get; set; } = default!;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 
     //protected override void OnInitialized()
     //{
+    //    Logger.LogInformation("Home Componente aufgerufen...");
+
     //    QotdViewModel = new QuoteOfTheDayViewModel("Larum lierum Löffelstiel", "Ich", "Dozent",
     //        DateOnly.FromDateTime(DateTime.Now), null, null);
     //}
